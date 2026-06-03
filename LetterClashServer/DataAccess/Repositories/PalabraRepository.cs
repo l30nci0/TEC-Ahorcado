9330601622278120ba56bf561511b9bb3256a13b
OGShawnLee <OGShawnLee@gmail.com>
2026-06-02 21:33:07 -0600

    feat: implement PalabraRepository and PalabraService using EF 6

---
+++ LetterClashServer/DataAccess/Repositories/PalabraRepository.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using LetterClashServer.DataAccess.Context;
+
+namespace LetterClashServer.DataAccess.Repositories {
+  public class PalabraRepository {
+    public List<Palabra> ObtenerPalabrasPorIdioma(string idioma) {
+      using (var context = new LetterClashDBEntities()) {
+        // Usamos AsNoTracking() para optimizar consultas de solo lectura
+        return context.Palabras
+                      .AsNoTracking()
+                      .Where(p => p.Idioma == idioma)
+                      .ToList();
+      }
+    }
+  }
+}
--- LetterClashServer/Services/PalabraService.svc.cs
+++ LetterClashServer/Services/PalabraService.svc.cs
@@ -1,11 +1,23 @@
 using System.Collections.Generic;
+using System.Linq;
 using LetterClashServer.Contracts;
 using LetterClashServer.Domain.Models;
+using LetterClashServer.DataAccess.Repositories;
 
 namespace LetterClashServer.Services {
   public class PalabraService : IPalabraService {
+    private readonly PalabraRepository palabraRepository = new PalabraRepository();
+
     public List<PalabraDTO> ObtenerPalabrasPorIdioma(string idioma) {
-      return new List<PalabraDTO>();
+      var palabras = palabraRepository.ObtenerPalabrasPorIdioma(idioma);
+       
+      // Nota: En C# la propiedad de la palabra se llama Palabra1 debido a la desambiguación de EF
+      return palabras.Select(p => new PalabraDTO {
+        IDPalabra = p.IDPalabra,
+        PalabraTexto = p.Palabra1,
+        Descripcion = p.Descripcion,
+        Idioma = p.Idioma
+      }).ToList();
     }
   }
 }
